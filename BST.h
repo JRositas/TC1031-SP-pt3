ac47dfecc06bb8f814ba0dbf89f0e5c2ecf2b0b9
JRositas <A00829760@itesm.mx>
2021-10-21 11:38:06 -0500

    Changed print
    Deleted upper comments, added complexity

--- BST.h
+++ BST.h
@@ -1,29 +1,15 @@
-/*
-El presente programa tiene el propósito que con las diversas funciones nos van a regresar un entero con el maxima anchura BST de enteros, 
-así como regresar el ancestro más cercano de dos datos, 
-de la misma manera que se crea un copy constructor de la clase BST que esta basada en un BST que se llega como entrada, 
-y por ultimo se haría la comparación de dos BST, sobrecargando el operador "==".
-*/
-
-
-// Matrícula: A00829821
-// Nombre: Ángela Felicia Guajardo Olveda
-// Fecha de creación: 16 Octubre 2021
 #include "NodeT.h"
 
-class BST
-{
+class BST{
 	public:
 		BST();
 		~BST();
 		void add(dataCS data);
-		void print(int tipo);	
-
+		void print();
 	private:
 		NodeT *root;
 		void inOrdenC(NodeT *r);
 		void destruye_Helper(NodeT *r);
-
 };
 
 BST::BST(){
@@ -42,7 +28,6 @@ void BST::destruye_Helper(NodeT *r){
 	}
 }
 
-
 // Complejidad: O(height)
 void BST::add(dataCS data){
 	if (root == nullptr){
@@ -58,36 +43,22 @@ void BST::add(dataCS data){
 			parent = curr;
 			curr = (curr->getData() > data) ? curr->getLeft() : curr->getRight();
 		}
-		// if (parent->getData() > data){
-		// 	parent->setLeft(new NodeT(data));
-		// }
-		// else{
-		// 	parent->setRight(new NodeT(data));
-		// }
 		(parent->getData() > data) ? parent->setLeft(new NodeT(data)) : parent->setRight(new NodeT(data));
 	}
 }
 
-
-
-// Tipo:
-// 1 : InOrdenC
-void BST::print(int tipo){
-	switch (tipo){
-		case 1: inOrdenC(root);
-				break;
-	}
+// Complejidad: O(n)
+void BST::print(){
+	inOrdenC(root);
 	cout << endl;
 }
-
 
 // Complejidad: O(n)
 void BST::inOrdenC(NodeT *r){
 	if (r != nullptr){
 		inOrdenC(r->getRight());
-		cout << r->getData() << " ";
+		dataCS dato = r->getData();
+		dato.print();
 		inOrdenC(r->getLeft());
 	}
 }
-
-
